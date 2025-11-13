0e601a94f0dbb1a4a37c07c9842c93056c72d6e1
Vince Park <vincepark@Vinces-iMac.modem>
2025-11-14 07:08:40 +1100

    basic practice files added

---
+++ discord-mafia-game-bot-main/practice2.py
@@ -0,0 +1,25 @@
+# This example requires the 'message_content' intent.
+
+
+# This example requires the 'message_content' intent.
+
+import discord
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello! :thumbsup:')
+
+client.run(bot_token)
